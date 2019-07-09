90aab79a49f0b770bc2be0af7d04ecfe0a1ba66f
UNIPLUSS\hakon.rydland <hakon.rydland@unipluss.no>
2019-07-09 10:40:22 +0200

    Ny iOS-metode

--- src/ios/v3SimplePlugin.m
+++ src/ios/v3SimplePlugin.m
@@ -25,4 +25,23 @@
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)toastMessage:(CDVInvokedUrlCommand*)command
+{
+NSString *message = @”iOS message from PhoneGap plugin v3SimplePlugin”;    
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:nil message:message  preferredStyle:UIAlertControllerStyleActionSheet];
+
+    [self presentViewController:alert animated:YES completion:nil];
+
+    int duration = 3;
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, duration * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+
+        [alert dismissViewControllerAnimated:YES completion:nil];
+
+    });
+}
+
 @end
+
+ 
